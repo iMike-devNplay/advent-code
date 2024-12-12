4f02fd7eeeae8e74f5c820d5fda18b592e8dc272
Michael Schneider <imike.devnplay@gmail.com>
2024-12-12 14:05:55 +0100

    day 12 wip

--- src/main/java/fr/home/mikedev/aoc2024/Main12.java
+++ src/main/java/fr/home/mikedev/aoc2024/Main12.java
@@ -6,15 +6,21 @@ import fr.home.mikedev.common.MainDay;
 
 public class Main12 extends MainDay 
 {
-
+	private char[][] puzzleMatrix = new char[10][10];
+	
 	public Main12(String title, String year) {super(title, year, "12");}
 	
 	public void retrieveData()
 	{
 		String line = null;	
+		int i = 0;
 		try(BufferedReader reader = this.getReader())
 		{
-			log(line);
+			while ((line = reader.readLine()) != null)
+			{
+				puzzleMatrix[i++] = line.toCharArray();
+				log(line);
+			}
 		}
 		catch (Exception ex)
 		{
